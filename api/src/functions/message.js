189467b16a0ae6eaefc9447350f09147678f3923
gartvi <gartvi@gmail.com>
2024-07-12 14:03:41 +0100

    json-3

--- api/src/functions/message.js
+++ api/src/functions/message.js
@@ -9,6 +9,6 @@ app.http('message', {
         const name = request.query.get('name') || await request.text() || 'world';
 
         //return {msg:'Hello, ${name}!'};
-        return {msg:'Hello!'};
+        return {msg:'Hello!', msg2: 'Another hello!'};
     }
 });
