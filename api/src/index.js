b7c467558dc21bc2e34059da49f8bc63b869fda8
gartvi <gartvi@gmail.com>
2024-07-12 15:44:36 +0100

    2 changes

--- api/src/index.js
+++ api/src/index.js
@@ -3,3 +3,9 @@ const { app } = require('@azure/functions');
 app.setup({
     enableHttpStream: true,
 });
+
+module.exports = async function (context, req) {
+    context.res.json({
+        text: "Hello from the API"
+    });
+};
