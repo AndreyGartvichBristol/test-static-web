4f157d035d45370efc0bc7170495888c9fae7cbd
gartvi <gartvi@gmail.com>
2024-07-12 16:20:24 +0100

    jss

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,3 @@
-const { app } = require('@azure/functions');
-
-app.setup({
-    enableHttpStream: true,
-});
-
 module.exports = async function (context, req) {
     context.res.json({
         text: "Hello from the API"
